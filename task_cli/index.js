61481be48d9d99910ade1562598ce34aab3bc04e
Sofroniy <Sofroniy.m@gmail.com>
2020-03-30 01:44:09 +0300

    add required options

--- task_cli/index.js
+++ task_cli/index.js
@@ -4,10 +4,10 @@ const { init } = require('./init');
 program.storeOptionsAsProperties(false);
 
 program
-  .option('-s, --shift <number>', 'a shift', parseInt)
+  .requiredOption('-s, --shift <number>', 'a shift', parseInt)
   .option('-i, --input <path>', 'an input file')
   .option('-o, --output <path>', 'an output file')
-  .option('-a, --action <string>', 'an action encode/decode')
+  .requiredOption('-a, --action <string>', 'an action encode/decode')
   .parse(process.argv);
 
 init(program.opts());
